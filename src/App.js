6e6e06d0d533807086b4163ce8558134f3c4f5b9
MarkkuVaara72 <markku.vaara@solenovo.fi>
2022-09-18 00:14:55 +0300

    Banner updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Box from './components/Box';
 const App = () => {
 
   const [page, setPage] = useState("start");
-  const [level, setLevel] = useState(4);
+  const [level, setLevel] = useState("");
 
   const changeLevel = (level) => {
     setLevel(level);
@@ -19,7 +19,7 @@ const App = () => {
     <div className="mainapp">
 
       <Title />
-      <Banner />
+      <Banner level={level} />
 
       <div className="main">
         {page === "start" 
@@ -30,15 +30,14 @@ const App = () => {
         {page === "level"
           && <div className="startbutton">
             <h2>Select level:</h2>
-            <button onClick={() => changeLevel(4)}>Apprentice</button>
-            <button onClick={() => changeLevel(6)}>Knight</button>
-            <button onClick={() => changeLevel(8)}>Master</button>
-            <button onClick={() => changeLevel(10)}>Grand Master</button>
+            <button onClick={() => changeLevel("Apprentice")}>Apprentice</button>
+            <button onClick={() => changeLevel("Knight")}>Knight</button>
+            <button onClick={() => changeLevel("Master")}>Master</button>
+            <button onClick={() => changeLevel("Grand Master")}>Grand Master</button>
           </div>
         }
         {page === "game"
           && <div className="boxes">
-            <p>Game level: {level}</p>
             <Box />
             <Box />
             <Box />
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,19 @@
 
-const Banner = () => (
+const Banner = ({level}) => (
 
     <div className="banner">
-        <p>(This is banner area)</p>
+        {!level &&
+            <p>Level not yet chosen</p>
+        }
+        {level &&
+            <h2>Level: {level}</h2>
+        }
+        {!level &&
+            <p>Points: 0</p>
+        }
+        {level &&
+            <h2>Points: 0</h2>
+        }
     </div>
 )
 
