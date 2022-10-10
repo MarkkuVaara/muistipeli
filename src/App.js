beb7f01d3806cf018867d334e5e6d29d989814b6
MarkkuVaara72 <markku.vaara@solenovo.fi>
2022-10-11 00:12:20 +0300

    Created simple startgrid and changed order of pages

--- src/App.js
+++ src/App.js
@@ -6,22 +6,38 @@ import Box from './components/Box';
 
 const App = () => {
 
-  const [page, setPage] = useState("start");
+  const [page, setPage] = useState("level");
   const [message, setMessage] = useState("");
 
   const [level, setLevel] = useState("");
   const [points, setPoints] = useState(0);
-  const [gridsize, setGridsize] = useState(0);
-  const [grid, setGrid] = useState([[]]);
+  const [gridsize, setGridsize] = useState(4);
+  const [grid, setGrid] = useState([]);
 
   const changeLevel = (level) => {
+
     setLevel(level);
     if (level === "Apprentice") { setGridsize(4); }
     if (level === "Knight") { setGridsize(6); }
     if (level === "Master") { setGridsize(8); }
     if (level === "Grand Master") { setGridsize(10); }
     setPoints(1000);
+    setPage("start");
+
+  }
+
+  const startGame = () => {
+
     setPage("game");
+
+    const gamegrid = [];
+    for (let i = 0; i < gridsize; i++) {
+      const gridrow = [];
+      let number = 1;
+      gamegrid[i] = number;
+    }
+    setGrid(gamegrid);
+  
   }
 
   return (
@@ -30,13 +46,9 @@ const App = () => {
 
       <Title />
       <Banner level={level} points={points} gridsize={gridsize} />
+      {grid}
 
       <div className="main">
-        {page === "start" 
-          && <div className="startbutton">
-            <button onClick={() => setPage("level")}>Start game</button>
-          </div>
-        }
         {page === "level"
           && <div className="startbutton">
             <h2>Select level:</h2>
@@ -44,6 +56,11 @@ const App = () => {
             <button onClick={() => changeLevel("Knight")}>Knight</button>
             <button onClick={() => changeLevel("Master")}>Master</button>
             <button onClick={() => changeLevel("Grand Master")}>Grand Master</button>
+          </div>
+        }
+        {page === "start" 
+          && <div className="startbutton">
+            <button onClick={() => startGame()}>Start game</button>
           </div>
         }
         {page === "game"
