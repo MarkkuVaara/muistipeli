abed7ea9f2270a7c2abf93c66b0f7d74f5d00675
Markku Vaara <markku.vaara45@gmail.com>
2022-10-14 23:22:30 +0300

    Changed card backside and other fixes

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,9 +1,9 @@
 
-import kukka from "../images/kukka.jpg";
+import backside from "../images/backside.jpg";
 
 const Box = () => (
     <div className="box">
-        <img alt="Profile" src={kukka} 
+        <img alt="Profile" src={backside} 
             style={{ width: '100%', height: '100%' }} />
     </div>    
 )
