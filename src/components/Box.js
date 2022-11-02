48dd0e3cff497c8e5590a724f36add1dbff6133e
Markku Vaara <markku.vaara45@gmail.com>
2022-11-02 21:53:28 +0200

    Added ripple effect for cards when clicked

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,10 +3,12 @@ import backside from "../images/backside.jpg";
 
 const Box = (props) => (
     <div className="flip-container">
+        <div className="flipper">
         <div className="box">
             <img alt="Profile" src={backside} 
                 style={{ width: '100%', height: '100%' }} />
             {props.picture}
+        </div>
         </div>
     </div>    
 )
